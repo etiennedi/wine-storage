0c43627237ec0af748e094124dc232300224a334
Dilocker Etienne <etienne.dilocker@metrosystems.net>
2017-05-21 00:09:19 +0200

    add insertion script to database

--- mock-data/index.js
+++ mock-data/index.js
@@ -14,7 +14,7 @@ const randomMinusOneToOne = () => 1 - (Math.random() * 2);
 
 const initialData = {
   logger_id: loggerId,
-  datetime: 1234567,
+  datetime: Date.now(),
   temperature: 10.0,
   humidity: 55,
 };
@@ -31,4 +31,29 @@ const data$ = Rx.Observable
       (randomMinusOneToOne() * maximumHumidityDifferencePerEntry),
   }), initialData);
 
-data$.subscribe(v => console.log('v', v));
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({ contactPoints: ['127.0.0.1'] });
+
+const insertStatement = `
+  INSERT INTO temperature_logger.temperature_log
+  (logger_id, datetime, temperature, humidity)
+  VALUES (?, ?, ?, ?)
+  `;
+
+const dbConnection = Rx.Observable.fromPromise(client.connect())
+  .switchMap(() => client.execute('TRUNCATE temperature_logger.temperature_log;'))
+  .mergeMap(() => data$)
+  .map(singleEntry => ({
+    query: insertStatement,
+    params: Object.values(singleEntry),
+  }))
+  .toArray()
+  .switchMap(queries => client.batch(queries, { prepare: true }))
+  .map(() => client.shutdown())
+  .catch((err) => {
+    console.error('There was an error when connecting', err);
+    return client.shutdown();
+  });
+
+dbConnection.subscribe();
