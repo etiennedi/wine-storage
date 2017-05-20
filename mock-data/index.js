eb679a0fd038c5f4b77081a7b8c2818781a851ba
Dilocker Etienne <etienne.dilocker@metrosystems.net>
2017-05-20 22:57:22 +0200

    add airbnb lint config and fix issues

--- mock-data/index.js
+++ mock-data/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const Rx = require('rxjs/Rx');
 
 const numberOfEntries = process.argv[2] || 100;
@@ -5,28 +6,29 @@ const averageSecondsBetweenSteps = process.argv[3] || 3600;
 
 console.log(`Creating ${numberOfEntries} entries with on average ${averageSecondsBetweenSteps} seconds between entries`);
 
-const logger_id = "fridge_0_zone_0"
+const loggerId = 'fridge_0_zone_0';
 const maximumTemperatureDifferencePerEntry = 0.1;
 const maximumHumidityDifferencePerEntry = 0.5;
 
-const randomMinusOneToOne =  () => 1 - (Math.random() * 2);
+const randomMinusOneToOne = () => 1 - (Math.random() * 2);
 
 const initialData = {
-  logger_id,
+  logger_id: loggerId,
   datetime: 1234567,
   temperature: 10.0,
   humidity: 55,
-}
+};
 
 const data$ = Rx.Observable
-  .range(1,numberOfEntries)
-  .scan((previous) => {
-    return {
-      logger_id,
-      datetime: previous.datetime + Math.floor(Math.random() * 2 * averageSecondsBetweenSteps),
-      temperature: previous.temperature + randomMinusOneToOne() * maximumTemperatureDifferencePerEntry,
-      humidity: previous.humidity + randomMinusOneToOne() * maximumHumidityDifferencePerEntry,
-    }
-  }, initialData)
+  .range(1, numberOfEntries)
+  .scan(previous => ({
+    logger_id: loggerId,
+    datetime: previous.datetime +
+      (Math.floor(Math.random() * 2 * averageSecondsBetweenSteps)),
+    temperature: previous.temperature +
+      (randomMinusOneToOne() * maximumTemperatureDifferencePerEntry),
+    humidity: previous.humidity +
+      (randomMinusOneToOne() * maximumHumidityDifferencePerEntry),
+  }), initialData);
 
-data$.subscribe( v=> console.log('v', v))
+data$.subscribe(v => console.log('v', v));
